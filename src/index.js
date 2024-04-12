80bfe5493e3cb5bb6c0bd85fce498f395917f44d
JerlsonSik <jlsonxyue@gmail.com>
2024-04-12 16:04:38 -0400

    AprilTwelve20242

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header';
-import Banner from './Banner';
 import reportWebVitals from './reportWebVitals';
-import HomeBannerTwo from './HomeBannerTwo';
-import HomeBannerThree from './HomeBannerThree';
-import HomeBannerFour from './HomeBannerFour';
-import HomeBannerFive from './HomeBannerFive';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Footer from './Footer';
-import Map from './Map';
-import HomeBannerSix from './HomeBannerSix';
-import HomeBannerSeven from './HomeBannerSeven';
-import SiteBanner from './SiteBanner';
-import SiteBannerTwo from './SiteBannerTwo';
-import SiteBannerThree from './SiteBannerThree';
-import RoomBanner from './RoomBanner';
-import RoomBannerTwo from './RoomBannerTwo';
-import AboutBanner from './AboutBanner';
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import App from './App';
 import 'bootstrap/dist/js/bootstrap.js';
